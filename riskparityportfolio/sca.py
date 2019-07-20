cca268e192a2fb2fd867b57bae74b5bb06e4c30e
mirca <jvmirca@gmail.com>
2019-07-20 19:29:56 -0300

    enh: add class SuccessiveConvexOptimizerValidator

--- riskparityportfolio/sca.py
+++ riskparityportfolio/sca.py
@@ -21,7 +21,7 @@ class RiskParityPortfolio:
         return self._weights
 
     @weights.setter
-    def weights(self, value)
+    def weights(self, value):
         if value is None:
             self._weights = tf.ones(self.number_of_assets) / self.number_of_assets
         try:
@@ -96,13 +96,10 @@ class RiskParityPortfolio:
         sca.solve()
 
 
-class SuccessiveConvexOptimizer:
-    """
-    Successive Convex Approximation optimizer taylored for the risk parity problem.
-    """
+class SuccessiveConvexOptimizerValidator:
 
-    def __init__(self, portfolio, tau = None, gamma = 0.9, zeta = 1E-7, funtol = 1E-6,
-                 wtol = 1E-6, maxiter = 5000):
+    def __init__(self):
+        pass
 
     @property
     def tau(self):
@@ -110,18 +107,108 @@ class SuccessiveConvexOptimizer:
 
     @tau.setter
     def tau(self, value):
-        if value is None:
-            self._tau = 0.05 * np.sum(np.diag(portfolio.covariance.numpy())) / (2 * portfolio.size)
+        if value < 0:
+            raise ValueError("tau has to be a positive real number, got {}.".format(value))
+        else:
+            try:
+                self._tau = float(value)
+            except e:
+                raise e
 
+    @property
+    def gamma(self):
+        return self._gamma
+
+    @gamma.setter
+    def gamma(self, value):
+        if not (0 < value < 1):
+            raise ValueError("gamma has to be a real number in the interval"
+                             "(0, 1), got {}.".format(value))
+        else:
+            try:
+                self._gamma = float(value)
+            except e:
+                raise e
+
+    @property
+    def zeta(self):
+        return self._zeta
+
+    @zeta.setter
+    def zeta(self, value):
+        if value < 0:
+            raise ValueError("zeta has to be a positive real number, got {}.".format(value))
+        else:
+            try:
+                self._zeta = float(value)
+            except e:
+                raise e
+
+    @property
+    def funtol(self):
+        return self._funtol
+
+    @funtol.setter
+    def funtol(self, value):
+        if value < 0:
+            raise ValueError("funtol has to be a positive real number, got {}.".format(value))
+        else:
+            try:
+                self._funtol = float(value)
+            except e:
+                raise e
+
+    @property
+    def wtol(self):
+        return self._wtol
+
+    @wtol.setter
+    def wtol(self, value):
+        if value < 0:
+            raise ValueError("wtol has to be a positive real number, got {}.".format(value))
+        else:
+            try:
+                self._wtol = float(value)
+            except e:
+                raise e
+    @property
+    def maxiter(self):
+        return self._maxiter
+
+    @maxiter.setter
+    def maxiter(self, value):
+        if value < 0:
+            raise ValueError("maxiter has to be a positive integer, got {}.".format(value))
+        else:
+            try:
+                self._maxiter = int(value)
+            except e:
+                raise e
 
     @property
     def qp_solver(self):
         return self._qp_solver
 
     @qp_solver.setter
-    def qp_solver(self, value)
+    def qp_solver(self, value):
         if value is None:
             self._qp_solver = osqp.OSQP()
+
+
+class SuccessiveConvexOptimizer:
+    """
+    Successive Convex Approximation optimizer taylored for the risk parity problem.
+    """
+    sca_validator = SuccessiveConvexOptimizerValidator()
+    def __init__(self, portfolio, tau = 1E-5, gamma = 0.9, zeta = 1E-7, funtol = 1E-6,
+                 wtol = 1E-6, maxiter = 5000):
+        self.tau       = sca_validator.tau     = tau
+        self.gamma     = sca_validator.gamma   = gamma
+        self.zeta      = sca_validator.zeta    = zeta
+        self.funtol    = sca_validator.funtol  = funtol
+        self.wtol      = sca_validator.wtol    = wtol
+        self.maxiter   = sca_validator.maxiter = maxiter
+        self.qp_solver = sca_validator.qp_solver
 
     def iterate(self):
         wk = portfolio.weights
@@ -142,9 +229,10 @@ class SuccessiveConvexOptimizer:
 
     def solve(self):
         i = 0
-        while(self.iterate() and i < maxiter) i += 1
+        while(self.iterate() and i < maxiter): i += 1
 
 
 def project_line_and_box(weights, lower_bound, upper_bound):
     def objective_function(variable, weights):
         pass
+    pass
