aea860985f549d6f40ecca141c770b6b234328bf
mirca <jvmirca@gmail.com>
2022-03-11 12:28:59 +0800

    add (Choi 2022) method

--- riskparityportfolio/tests/test_vanilla_rpp.py
+++ riskparityportfolio/tests/test_vanilla_rpp.py
@@ -1,19 +1,22 @@
 import riskparityportfolio as rpp
 import numpy as np
+import pytest
 
-
-def test():
-    n = 1000
+@pytest.mark.parametrize("method", ["choi", "spinu"])
+def test(method):
+    n = 100
     t = 1000 * n
     x = np.random.normal(size=t).reshape((n, -1))
-    corr = x @ x.T / t
-    b = np.ones(len(corr)) / len(corr)
-    w = rpp.vanilla.design(corr, b)
-    rc = w @ (corr * w)
+    cov = np.cov(x)
+    print(cov.shape)
+    b = np.ones(len(cov)) / len(cov)
+    w = rpp.vanilla.design(cov, b, maxiter=1000, method=method)
+    print(w)
+    rc = w @ (cov * w)
     rc = rc / np.sum(rc)
     # assert that the portfolio respect the budget constraint
     np.testing.assert_almost_equal(np.sum(w), 1)
-    # assert that the portfolio repect the no-shortselling constraint
-    np.testing.assert_equal(all(w > 0), True)
+    # assert that the portfolio respect the no-shortselling constraint
+    np.testing.assert_equal(all(w >= 0), True)
     # assert that the desired risk contributions are attained
-    np.testing.assert_allclose(rc, b, rtol=1e-4)
+    np.testing.assert_allclose(rc, b, atol=1/(10*n))
